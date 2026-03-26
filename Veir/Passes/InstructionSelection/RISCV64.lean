454521659c827c73a7006e6a8d131f20d968f455
Luisa Cicolini <48860705+luisacicolini@users.noreply.github.com>
2026-03-26 08:43:46 +0000

    `li` isel pass (#333)
    we add a pattern to lower `llvm.add` to `riscv.add` for `i64` arguments.
    
    At the moment, we only lower `i64` types and ignore everything else. In
    a follow-up PR we will add explicit errors for unsupported types,
    instead of ignoring them. A few changes are necessary for this, and
    after discussing this with @math-fehr we decided to have them in a
    separate PR.
    
    For completeness, we should insert a casting operation that, given an
    argument of any type `i*`, casts it to `i64`, which is the type we
    reference in our `reg` definition.

---
+++ Veir/Passes/InstructionSelection/RISCV64.lean
@@ -0,0 +1,43 @@
+import Veir.Pass
+import Veir.PatternRewriter.Basic
+import Veir.Passes.InstCombine
+
+namespace Veir
+
+/-!
+  This file replicates LLVM's GlobalISel instruction selector,
+  to lower LLVM IR to RISC-V assembly (64 bits).
+-/
+
+/-! # Lowering Patterns -/
+
+set_option warn.sorry false in
+/-- llvm.constant -> riscv.li -/
+def constant (rewriter: PatternRewriter OpCode) (op: OperationPtr) :
+    Option (PatternRewriter OpCode) := do
+  let some const := matchConstantOp op rewriter.ctx
+      | return rewriter
+  if const.type.bitwidth ≠ 64 then return rewriter
+  let type := ((op.getResult 0).get! rewriter.ctx).type
+  let .integerType type' := type.val | rewriter
+  if type'.bitwidth ≠ 64 then return rewriter
+  let (rewriter, newOp) ← rewriter.createOp .riscv_li #[type] #[]
+      #[] #[] {value := const} (some $ .before op) sorry sorry sorry sorry
+  rewriter.replaceOp op newOp sorry sorry sorry
+
+/-! # Pass implementation -/
+
+set_option warn.sorry false in
+def ISelPass.impl (ctx : { ctx' : IRContext OpCode // ctx'.WellFormed }) (op : OperationPtr)
+    (_ : op.InBounds ctx.val) :
+    ExceptT String IO { ctx' : IRContext OpCode // ctx'.WellFormed } := do
+  let pattern := RewritePattern.GreddyRewritePattern #[constant]
+  match RewritePattern.applyInContext pattern ctx ctx.property.inBounds with
+  | none => throw "Error while applying pattern rewrites"
+  | some ctx => pure ⟨ctx, sorry⟩
+
+public def IselRISCV64 : Pass OpCode :=
+  { name := "isel-riscv64"
+    description :=
+      "Lower LLVM IR to RISCV 64 assembly instruction selection pass."
+    run := ISelPass.impl }
