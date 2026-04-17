04819a45d8d72bddc4c2eb025a5017b144972af6
Axel Sorenson <AxelPSorenson@gmail.com>
2026-04-16 19:56:53 -0600

    Cleanup (WIP)

--- Veir/IR/Dominance.lean
+++ Veir/IR/Dominance.lean
@@ -10,19 +10,29 @@ public section
 
 variable {OpInfo : Type} [HasOpInfo OpInfo]
 
+/-- Pointer to a node in a dominance tree.
+Contains a RegionPtr to indicate which dominance 
+tree this node belongs to.
+Contains an index which points to the DomTreeNode
+in question.
+-/
 structure DomTreeNodePtr where
   region : RegionPtr
   index : Nat
 deriving Inhabited, Repr, DecidableEq, Hashable
 
+/-- An MLIR block pointer with dominance information.
+Contains its immediate dominator and the children it dominates.
+Invariants:
+If the firstChild is none, lastChild is none.
+If firstChild is some, lastChild is some.
+lastChild's sibling should always be none.
+-/
 structure DomTreeNode where
-  -- The block in question
   block : BlockPtr
-  -- The immediate dominator of the block
   iDom : Option DomTreeNodePtr
-
-  firstChild : Option DomTreeNodePtr -- Invariant: If firstChild is none, lastChild is none. If firstChild is some, lastChild is some.
-  lastChild : Option DomTreeNodePtr -- Invariant: lastChild's sibling should always be none
+  firstChild : Option DomTreeNodePtr
+  lastChild : Option DomTreeNodePtr
   sibling : Option DomTreeNodePtr
   deriving Inhabited, Repr
 
@@ -32,24 +42,23 @@ abbrev DomContext := HashMap RegionPtr DomTree
 instance : Repr DomContext where
   reprPrec ctx prec := reprPrec (ctx.toList) prec
 
-def DomTreeNode.new (block : BlockPtr) (iDom : Option DomTreeNodePtr) : DomTreeNode :=
+def DomTreeNode.empty (block : BlockPtr) : DomTreeNode :=
 {
   block := block
-  iDom := iDom 
+  iDom := none
   firstChild := none
   lastChild := none
   sibling := none
 }
 
-def RegionPtr.getDomTree! (ptr : RegionPtr) (ctx : DomContext) : DomTree := ctx[ptr]!
+def RegionPtr.getDomTree! (ptr : RegionPtr) (ctx : DomContext) : DomTree := 
+  ctx[ptr]!
 
 def RegionPtr.newDomTreeNode! (ptr : RegionPtr) (block : BlockPtr) (ctx : DomContext) : DomContext := 
-  let tree := (ptr.getDomTree! ctx).push (DomTreeNode.new block none)
-  ctx.insert ptr tree
+  ctx.modify ptr fun tree => tree.push (DomTreeNode.empty block)
 
 def RegionPtr.getDomTreeSize! (ptr : RegionPtr) (ctx : DomContext) : Nat :=
-  let tree := (ptr.getDomTree! ctx)
-  tree.size
+  (ptr.getDomTree! ctx).size
 
 def DomTreeNodePtr.getDomTree! (ptr : DomTreeNodePtr) (ctx : DomContext) : DomTree :=
   ptr.region.getDomTree! ctx
@@ -78,10 +87,8 @@ def DomTreeNodePtr.getLastChildSibling! (ptr : DomTreeNodePtr) (ctx : DomContext
 def DomTreeNodePtr.isLeaf! (ptr : DomTreeNodePtr) (ctx : DomContext) : Bool :=
   (ptr.getFirstChild! ctx).isNone
 
-def DomContext.updateNode! (ptr : DomTreeNodePtr) (f : DomTreeNode -> DomTreeNode) (ctx : DomContext) : DomContext :=
-  let tree := ptr.getDomTree! ctx
-  let tree' := tree.set! ptr.index (f (ptr.get! ctx))
-  ctx.insert ptr.region tree'
+def DomContext.modifyNode! (ctx : DomContext) (ptr : DomTreeNodePtr) (f : DomTreeNode -> DomTreeNode) : DomContext :=
+  ctx.modify ptr.region fun tree => tree.set! ptr.index (f (ptr.get! ctx))
 
 def DomTreeNodePtr.addChild! (parent child : DomTreeNodePtr) (ctx : DomContext) : DomContext := 
   let parentNode := parent.get! ctx 
@@ -93,10 +100,10 @@ def DomTreeNodePtr.addChild! (parent child : DomTreeNodePtr) (ctx : DomContext) 
     panic! "sibling of last child must be none" 
   else
     match parentNode.lastChild with
-    | none => ctx.updateNode! parent fun n => { n with firstChild := some child, lastChild := some child }
+    | none => ctx.modifyNode! parent ({ · with firstChild := some child, lastChild := some child })
     | some last => 
-      let ctx := ctx.updateNode! last fun n => { n with sibling := some child }
-      ctx.updateNode! parent fun parentNode => { parentNode with lastChild := some child }
+      let ctx := ctx.modifyNode! last ({ · with sibling := some child })
+      ctx.modifyNode! parent ({ · with lastChild := some child })
 
 def DomTreeNodePtr.removeChild! (parent child : DomTreeNodePtr) (ctx : DomContext) : DomContext := Id.run do
   let childSibling := child.getSibling! ctx
@@ -113,13 +120,12 @@ def DomTreeNodePtr.removeChild! (parent child : DomTreeNodePtr) (ctx : DomContex
   -- Special case: child being removed is first child in sibling list
   if parentFirst = child then
     if parentFirst = parentLast then
-      ctx.updateNode! parent fun n => { n with firstChild := none, lastChild := none }
+      ctx.modifyNode! parent ({ · with firstChild := none, lastChild := none })
     else
-      let ctx := ctx.updateNode! parent fun n => { n with firstChild := childSibling }
-      ctx.updateNode! child fun n => { n with sibling := none } 
+      let ctx := ctx.modifyNode! parent fun n => { n with firstChild := childSibling }
+      ctx.modifyNode! child ({ · with sibling := none })
   else -- Iterate
-    let prev := parentFirst
-    match prev with
+    match parentFirst with
     | none => panic! "Not in immediate dominator children list!"
     | some prev => 
         let mut prev := prev
@@ -128,41 +134,42 @@ def DomTreeNodePtr.removeChild! (parent child : DomTreeNodePtr) (ctx : DomContex
           match curr with
           | none => panic! "Not in immediate dominator children list!"
           | some sibling => prev := sibling; curr := (sibling.getSibling! ctx) 
-        let ctx := ctx.updateNode! prev fun n => { n with sibling := childSibling }
+        let ctx := ctx.modifyNode! prev fun n => { n with sibling := childSibling }
         if childSibling.isSome then
-          ctx.updateNode! child fun n => { n with sibling := none }
+          ctx.modifyNode! child ({ · with sibling := none })
         else
-          ctx.updateNode! parent fun n => { n with lastChild := prev }
+          ctx.modifyNode! parent ({ · with lastChild := prev })
 
-  def DomTreeNodePtr.setIDom! (ptr newIDom : DomTreeNodePtr) (ctx : DomContext) : DomContext := Id.run do
-    match ptr.getIDom! ctx with
-    | none =>
-      let ctx := DomContext.updateNode! ptr (fun n => { n with iDom := some newIDom }) ctx
+def DomTreeNodePtr.setIDom! (ptr newIDom : DomTreeNodePtr) (ctx : DomContext) : DomContext := Id.run do
+  match ptr.getIDom! ctx with
+  | none =>
+    let ctx := ctx.modifyNode! ptr ({ · with iDom := some newIDom })
+    newIDom.addChild! ptr ctx
+  | some iDom =>
+    if iDom = newIDom then 
+      return ctx
+    else
+      let ctx := iDom.removeChild! ptr ctx
+      let ctx := ctx.modifyNode! ptr ({ · with iDom := some newIDom })
       newIDom.addChild! ptr ctx
-    | some iDom =>
-      if iDom = newIDom then 
-        return ctx
-      else
-        let ctx := iDom.removeChild! ptr ctx
-        let ctx := DomContext.updateNode! ptr (fun n => { n with iDom := some newIDom }) ctx
-        newIDom.addChild! ptr ctx
 
--- Uses the Cooper Harvey Kennedy algorithm
+def intersect (block1 block2 : BlockPtr) (idx : HashMap BlockPtr DomTreeNodePtr) (domCtx : DomContext) : BlockPtr := Id.run do 
+  let mut finger1 := idx[block1]!
+  let mut finger2 := idx[block2]!
+  while finger1 != finger2 do
+    while finger1.index < finger2.index do
+      finger1 := (finger1.getIDom! domCtx).get! 
+    while finger2.index < finger1.index do  
+      finger2 := (finger2.getIDom! domCtx).get! 
+  return (finger1.getBlock! domCtx)
+
+/-- Constructs a dominance tree for a region.
+Uses the Cooper Harvey Kennedy algorithm.
+-/
 def RegionPtr.computeDomTree! (ptr : RegionPtr) (domCtx : DomContext) (irCtx : IRContext OpInfo) : DomContext := Id.run do
-  let intersect (block1 : BlockPtr) (block2 : BlockPtr) (idx : HashMap BlockPtr DomTreeNodePtr) (domCtx : DomContext) : BlockPtr := Id.run do 
-    let mut finger1 := idx[block1]!
-    let mut finger2 := idx[block2]!
-    while finger1 != finger2 do
-      while finger1.index < finger2.index do
-        finger1 := (finger1.getIDom! domCtx).get! 
-      while finger2.index < finger1.index do  
-        finger2 := (finger2.getIDom! domCtx).get! 
-    return (finger1.getBlock! domCtx)
-
   -- Postorder traversal of blocks, insert into DomTree (which is just an array!) 
   let mut postOrderIndex : HashMap BlockPtr DomTreeNodePtr := {}
-  let mut domCtx := domCtx
-  domCtx := domCtx.insert ptr #[]  
+  let mut domCtx := domCtx.insert ptr #[]
   match (ptr.get! irCtx).firstBlock with
   | none => return domCtx
   | some entry =>
@@ -188,7 +195,7 @@ def RegionPtr.computeDomTree! (ptr : RegionPtr) (domCtx : DomContext) (irCtx : I
 
     -- Give entry block its iDom (which is itself)
     let entryPtr := postOrderIndex[entry]!
-    domCtx := DomContext.updateNode! entryPtr (fun n => { n with iDom := some entryPtr }) domCtx
+    domCtx := DomContext.modifyNode! domCtx entryPtr ({ · with iDom := some entryPtr })
 
     -- Iterate backwards through the DomTree (reverse postorder traversal)
     let mut changed := true
