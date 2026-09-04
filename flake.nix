65e5b233883c4d50f834d531b489839c4fe474f0
Axel Sorenson <AxelPSorenson@gmail.com>
2026-09-04 07:44:46 -0600

    feat: add nix flake

---
+++ flake.nix
@@ -0,0 +1,76 @@
+{
+  description = "Verified Intermediate Representation";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+
+      developmentPackages = pkgs: with pkgs; [
+        bash
+        clang
+        elan
+        gmp
+        gnumake
+        llvmPackages.llvm
+        pkg-config
+        uv
+      ];
+
+      makeApp = pkgs: target:
+        let
+          suffix = if target == null then "help" else target;
+          command = if target == null then "make" else "make ${target}";
+          package = pkgs.writeShellApplication {
+            name = "veir-${suffix}";
+            runtimeInputs = developmentPackages pkgs;
+            text = ''
+              export LEAN_AR="${pkgs.llvmPackages.llvm}/bin/llvm-ar"
+              export LEAN_CC="${self}/ExArray/compiler"
+              exec ${command} "$@"
+            '';
+          };
+        in
+        {
+          type = "app";
+          program = "${package}/bin/veir-${suffix}";
+          meta.description =
+            if target == null
+            then "Show VeIR's Make targets"
+            else "Run VeIR's make ${target} target";
+        };
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = developmentPackages pkgs;
+
+            # ExArray uses Clang LTO and therefore needs its compiler wrapper
+            # together with LLVM's archiver.
+            LEAN_AR = "${pkgs.llvmPackages.llvm}/bin/llvm-ar";
+            LEAN_CC = "${self}/ExArray/compiler";
+          };
+        });
+
+      apps = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = makeApp pkgs null;
+          build = makeApp pkgs "build";
+          tests = makeApp pkgs "tests";
+        });
+    };
+}
